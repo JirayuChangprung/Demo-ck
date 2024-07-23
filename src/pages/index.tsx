a34fd7e60be791ed488a3c494a5f4ccc314de47d
JirayuChangprung <143163918+JirayuChangprung@users.noreply.github.com>
2024-07-23 16:00:21 +0700

    index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,20 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import styles from '../style/home.module.css';
-import { useRouter } from 'next/router'; // useRouter
+import { useRouter } from 'next/router';
 import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
 const Editor = dynamic(() => import('../components/Editor'), { ssr: false });
 
 const Home: React.FC = () => {
     const [editorData, setEditorData] = useState('');
-    const router = useRouter(); // router
+    const router = useRouter();
 
-    const handleEditorChange = (event: Event, editor: any) => {
+    const handleEditorChange = (event: any, editor: any) => {
         const data = editor.getData();
         setEditorData(data);
     };
 
-    // ฟังก์ชันปุ่ม
     const handleButtonClick = () => {
         router.push({
             pathname: '/output',
@@ -27,7 +26,7 @@ const Home: React.FC = () => {
     return (
         <div className={styles.home}>
             <h1>CKEditor</h1>
-            <div className={styles['main-container']} >
+            <div className={styles['main-container']}>
                 <Editor value={editorData} onChange={handleEditorChange} />
             </div>
             <div>
@@ -36,7 +35,6 @@ const Home: React.FC = () => {
                 </button>
             </div>
         </div>
-        
     );
 };
 
